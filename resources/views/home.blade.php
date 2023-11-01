4d5a7be7f45a61e2bb1e61a3a61695162e31782e
Ian <ianreeves19981@gmail.com>
2023-11-01 22:09:34 +0000

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title')
+
+@section('content')
+
+        <div class="hero">
+            <img src="/img/CastleGlowCompleted.png" alt="" class="img-hero">
+            <p class="centered-text">Hello</p>
+        </div>
+
+        <div class="container">
+            tett
+        </div>
+@endsection
