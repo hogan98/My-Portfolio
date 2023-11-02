ba7312046fb02b602dc0598516b46ca4261a49a7
Ian <ianreeves19981@gmail.com>
2023-11-02 00:11:27 +0000

    Working on nav almost Complete need to do Hover effect and toggle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,12 @@
 
         <div class="hero">
             <img src="/img/CastleGlowCompleted.png" alt="" class="img-hero">
-            <p class="centered-text">Hello</p>
+            <div class="centered-text">
+                <h1>Something line Hello, Im Ian Reeves.
+                Im a full-stack web developer
+                </h1>
+                <button class="outline-dotted mt-4 p-2">View My Work -></button>
+            </div>
         </div>
 
         <div class="container">
