af5769528b2f1ba24b907a9e29b97d3a861229f6
Ian <ianreeves19981@gmail.com>
2023-11-04 18:38:05 +0000

    Added button animation and started grid, also did some nav work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,35 @@
         <div class="hero">
             <img src="/img/CastleGlowCompleted.png" alt="" class="img-hero">
             <div class="centered-text">
-                <h1>Something line Hello, Im Ian Reeves.
+                <h1 class="hero-text">Hello, Im <span class="title">IAN REEVES.</span>
                 Im a full-stack web developer
                 </h1>
-                <button class="outline-dotted mt-4 p-2">View My Work -></button>
+                <button class="hero-btn mt-4 p-3">View My Work</button>
             </div>
         </div>
 
         <div class="container">
-            tett
+            
+        
+        <div class="main-info-container">
+                <p>this is a test Main-Info-Container</p>
+            
+            <div class="info-img-container">
+                <p>this is a test Img-container</p>
+            </div>
+            <div class="info-container">
+                <p>This is a test Info-container</p>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div>
+
         </div>
 @endsection
