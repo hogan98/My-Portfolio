5cee17a3e7010471bdbdf36fb6b17f96ac466a37
Ian <ianreeves19981@gmail.com>
2023-11-11 19:17:51 +0000

    Navbar Completed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
-import './bootstrap';
+import "./bootstrap";
+const hamburger = document.querySelector(".hamburger");
+const imgHero = document.querySelector(".img-hero");
+
+function adjustHeroMargin() {
+    const windowWidth = window.innerWidth;
+    const navBar = document.querySelector(".nav-bar");
+
+    if (windowWidth <= 900 && navBar.classList.contains("active")) {
+        imgHero.style.marginTop = "380px";
+    } else {
+        imgHero.style.marginTop = "0";
+    }
+}
+
+hamburger.onclick = function () {
+    const navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+    adjustHeroMargin(); // Call the adjustment function on hamburger click
+};
+
+// Add an event listener to handle window resize
+window.addEventListener("resize", adjustHeroMargin);
+
+// Normal Version with no Hero Image
+// const hamburger = document.querySelector(".hamburger");
+// hamburger.onclick = function () {
+//     const navBar = document.querySelector(".nav-bar");
+//     const imgHero = document.querySelector(".img-hero");
+//     navBar.classList.toggle("active");
+
+// };
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,19 +8,16 @@
             <img src="/img/CastleGlowCompleted.png" alt="" class="img-hero">
             <div class="centered-text">
                 <h1 class="hero-text">Hello, Im <span class="title">IAN REEVES.</span>
-                Im a full-stack web developer
+                Im a full-stack web developer.
                 </h1>
                 <button class="hero-btn mt-4 p-3">View My Work</button>
             </div>
         </div>
 
         <div class="container">
-            
-            
-            <div class="main-info-container">
-                
+            <div class="main-info-container mb-16">
                 <div class="info-img-container">
-                    <img src="/img/castlefade.png" alt="" class="text-image">
+                    <img src="/img/castleglowcompleted.png" alt="" class="text-image">
                 </div>
                 <div class="info-container">
                     <h2 class="text-2xl font-medium mt-5 text-center">Lorem ipsum dolor sit amet.</h2>
@@ -29,14 +26,106 @@
                 </div>
             </div>
 
+            <p class="headings">Web Portfolio</p>
             <div class="card-container">
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
+                <div class="card">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="/img/castleglowcompleted.png" alt="" />
+                    </a>
+                    <div class="card-content">
+                        <h5 class="card-title">An Dulra</h5>
+                        <p class="card-text">test</p>
+                        <a href="" class="card-link">
+                            Read More
+                         <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img  class="rounded-t-lg" src="/img/castleglowcompleted.png" alt="">
+                    </a>
+                    <div class="card-content">
+                        <h5 class="card-title">Wimbledon Wine School</h5>
+                        <p class="card-text">test</p>
+                        <a href="" class="card-link">
+                            Read More
+                         <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img  class="rounded-t-lg"src="/img/castleglowcompleted.png" alt="">
+                    </a>
+                    <div class="card-content">
+                        <h5 class="card-title">Nz Protein</h5>
+                        <p class="card-text">test</p>
+                        <a href="" class="card-link">
+                            Read More
+                         <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img  class="rounded-t-lg"src="/img/castleglowcompleted.png" alt="">
+                    </a>
+                    <div class="card-content">
+                        <h5 class="card-title">Castletroy Hockey Club</h5>
+                        <p class="card-text">test</p>
+                        <a href="" class="card-link">
+                            Read More
+                         <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img  class="rounded-t-lg"src="/img/castleglowcompleted.png" alt="">
+                    </a>
+                    <div class="card-content">
+                        <h5 class="card-title">Anywair</h5>
+                        <p class="card-text">test</p>
+                        <a href="" class="card-link">
+                            Read More
+                         <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img  class="rounded-t-lg"src="/img/butterflyglow.png" alt="">
+                    </a>
+                    <div class="card-content">
+                        <h5 class="card-title">Gaming Nirvana</h5>
+                        <p class="card-text">test</p>
+                        <a href="" class="card-link">
+                            Read More
+                         <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                        </a>
+                    </div>
+                </div>
             </div>
 
+            <p class="headings">Graphic Portfolio</p>
+            
         </div>
 @endsection
