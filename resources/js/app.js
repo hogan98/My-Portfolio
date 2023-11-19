e1a57ced5cdd2bb123bc73694d6a42215a439ae2
Ian <ianreeves19981@gmail.com>
2023-11-19 23:44:19 +0000

    Changed made to card, contact fornm and footer completed

--- resources/js/app.js
+++ resources/js/app.js
@@ -61,3 +61,22 @@ prevBtn.addEventListener("click", () => {
     slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
     showSlide();
 });
+
+//Scroll To Top BUton
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (
+        document.body.scrollTop > 30 ||
+        document.documentElement.scrollTop > 30
+    ) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,25 +9,27 @@
                 <h1 class="hero-text">Hello, Im <span class="title">IAN REEVES.</span>
                 Im a full-stack web developer.
                 </h1>
-                <button class="hero-btn mt-4 p-3">View My Work</button>
+                <button class="hero-btn mt-4 p-3" id="#web-portfoliob">View My Work</button>
             </div>
         </div>
 
         <div class="container">
             <div class="main-info-container mb-16">
-                <div class="info-img-container">
+                <div id="about" class="info-img-container">
                     <img src="/img/castleglowcompleted.png" alt="" class="text-image">
                     <p class="mobile-heading">About Me</p>
                     <p class="mobile-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus mollitia eum nesciunt soluta ut magni quo odit dicta rem consequatur.</p>
                 </div>
                 <div class="info-container">
                     <h2 class="section-headings-bg-2">About Me</h2>
-                    <p class="text-centered">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus mollitia eum nesciunt soluta ut magni quo odit dicta rem consequatur.</p>
-                    
+                    <p class="text-centered">From 2017 to 2020, I attended Technological University of the Shannon, where I acquired valuable skills in software development. Since then, I have dedicated myself to honing my expertise in various areas, including graphic design, HTML, CSS, JavaScript, React, and Laravel. My goal is to leverage my skills to work on full-stack projects, encompassing graphic design, front-end development, and back-end development.
+
+Currently, I am employed at an Irish-based web agency, but I am open to exploring new opportunities, including freelance projects.</p>
+                    Tech Stack Here
                 </div>
             </div>
 
-            <p class="headings">Web Portfolio</p>
+            <p id="web-portfolio" class="headings">Web Portfolio</p>
             <div class="card-container">
                 <div class="card">
                     <a href="#">
@@ -35,9 +37,8 @@
                     </a>
                     <div class="card-content">
                         <h5 class="card-title">An Dulra</h5>
-                        <p class="card-text">test</p>
                         <a href="" class="card-link">
-                            Read More
+                           View Site
                          <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path class="card-icon-path" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
@@ -51,7 +52,6 @@
                     </a>
                     <div class="card-content">
                         <h5 class="card-title">Wimbledon Wine School</h5>
-                        <p class="card-text">test</p>
                         <a href="" class="card-link">
                             Read More
                          <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -67,7 +67,6 @@
                     </a>
                     <div class="card-content">
                         <h5 class="card-title">Nz Protein</h5>
-                        <p class="card-text">test</p>
                         <a href="" class="card-link">
                             Read More
                          <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -83,7 +82,6 @@
                     </a>
                     <div class="card-content">
                         <h5 class="card-title">Castletroy Hockey Club</h5>
-                        <p class="card-text">test</p>
                         <a href="" class="card-link">
                             Read More
                          <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -99,7 +97,6 @@
                     </a>
                     <div class="card-content">
                         <h5 class="card-title">Anywair</h5>
-                        <p class="card-text">test</p>
                         <a href="" class="card-link">
                             Read More
                          <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -115,7 +112,6 @@
                     </a>
                     <div class="card-content">
                         <h5 class="card-title">Gaming Nirvana</h5>
-                        <p class="card-text">test</p>
                         <a href="" class="card-link">
                             Read More
                          <svg class="card-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -126,7 +122,7 @@
                 </div>
             </div>
             
-            <p class="headings mt-14">Graphic Portfolio</p>
+            <p id="graphic-portfolio" class="headings mt-14">Graphic Portfolio</p>
             <div class="carousel">
                 <div class="carousel-container">
                         <div class="carousel-slide">
@@ -159,19 +155,32 @@
                 </div>
             </div>
 
-            <p class="headings mt-14">Contact</p>
-            <div class="contact-form">
-            <div class="form-info">
-                <p class="contact-header">Contact me at:</p>
-                <p class="contact-text">Email:</p>
-                <p class="contact-text">Or submit form -> </p>
+            <button id="scrollToTopBtn" class="scroll-to-top-btn" title="Go to top">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+                    <path d="M0 0h24v24H0V0z" fill="none"/>
+                    <path d="M12 2c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm5 13h-3v6h-4v-6H7l5-5 5 5z"/>
+                </svg>
+            </button>
+
+            <div id="contact" class="contact-box">
+                <p class="headings mt-14">Contact</p>
+                <div class="contact-form">
+                    <div class="form-info">
+                        <p class="contact-header">Contact me at:</p>
+                        <p class="contact-text mt-5">Contact via Email: <a href="mailto:ianreeves19981@gmail.com">ianreeves19981@gmail.com</a></p>
+                        <p class="contact-text">Alternatively, complete the form below :</p>
+                    </div>
+                    <form action="#">
+                        <input type="text" placeholder="Your Name" class="">
+                        <input type="email" placeholder="Email Address" class="">
+                        <textarea name="message" placeholder="Message(Optional)" cols="30" rows="10"></textarea>
+                        <button type="submit" class="contact-btn ">Send Message</button>
+                    </form>
+                </div>
             </div>
-            <form action="#">
-                <input type="text" placeholder="Your Name" class="">
-                <input type="email" placeholder="Email Address" class="">
-                <textarea name="message" placeholder="Message(Optional)" cols="30" rows="10"></textarea>
-                <button type="submit" class="contact-btn ">Send Message</button>
-            </form>
-            </div>
+
+        </div>
+
+        <div class="background-shape">
         </div>
 @endsection
